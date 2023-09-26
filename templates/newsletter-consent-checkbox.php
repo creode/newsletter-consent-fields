7096dfb2d084d557783abfee2a85647fe8921404
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-09-26 10:16:45 +0100

    Use field title as key.

--- templates/newsletter-consent-checkbox.php
+++ templates/newsletter-consent-checkbox.php
@@ -8,7 +8,7 @@
 ?>
 
 <p class="form-row form-row-wide newsletter-consent">
-	<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="newsletter_consent_field" type="checkbox" name="<?php echo sanitize_key( $post_key ); ?>" value="1">
+	<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="<?php echo sanitize_key( $post_key ); ?>" type="checkbox" name="<?php echo sanitize_key( $post_key ); ?>" value="1">
 	<label for="<?php echo sanitize_key( $post_key ); ?>" class="woocommerce-form__label woocommerce-form__label-for-checkbox inline">
 		<?php
 
