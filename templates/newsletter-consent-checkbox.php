c8cc8081201d5bb30184d8348965a84ca20a9996
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-09-26 13:41:07 +0100

    Adds functionality to retain checked state when validation errors occur.

--- templates/newsletter-consent-checkbox.php
+++ templates/newsletter-consent-checkbox.php
@@ -5,10 +5,13 @@
  * @package Newsletter_Consent_Fields
  */
 
+// phpcs:ignore WordPress.Security.NonceVerification.Missing
+$is_checked = isset( $_POST[ $post_key ] ) ? (bool) $_POST[ $post_key ] : false;
+
 ?>
 
 <p class="form-row form-row-wide newsletter-consent">
-	<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="<?php echo sanitize_key( $post_key ); ?>" type="checkbox" name="<?php echo sanitize_key( $post_key ); ?>" value="1">
+	<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="<?php echo sanitize_key( $post_key ); ?>" type="checkbox" name="<?php echo sanitize_key( $post_key ); ?>" value="1" <?php echo $is_checked ? 'checked' : ''; ?> />
 	<label for="<?php echo sanitize_key( $post_key ); ?>" class="woocommerce-form__label woocommerce-form__label-for-checkbox inline">
 		<?php
 
